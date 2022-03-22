88d1e7c62e831caba1fdbb9372e3e486b7ec0a36
Camille <camillemariuschatton@gmail.com>
2022-03-22 07:45:25 +0100

    added menu

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -21,3 +21,28 @@ class Body extends StatelessWidget {
     );
   }
 }
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
+  int selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) =>
+            Text(
+              categories[index],
+            ),
+      ),
+    );
+  }
+}
